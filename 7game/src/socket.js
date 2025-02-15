edc8f40c0da8b6401b574f7014328caac3d5d266
ChristianSteen-SW23 <cniel@student.aau.dk>
2025-02-15 21:41:26 +0100

    Added env file

--- 7game/src/socket.js
+++ 7game/src/socket.js
@@ -4,14 +4,16 @@ Note: The following document here uses guidelines from:
 */
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.MODE === "production" ? "http://49.13.58.73:3069" : "http://localhost:3069";
+const SOCKET_IP = import.meta.env.VITE_FRONT_SOCKET_IP || "localhost:3069";
+console.log("IP", SOCKET_IP)
+const URL = import.meta.env.MODE === "production" ? `http://${SOCKET_IP}` : "http://localhost:3069";
 
 export const socket = io(URL);
 
 /* The following might be needed when deploying to production,
 as socket.io makes use of different protocols. http
 */
-//export const socket = io(URL, {
-//  transports: ['websocket'],
-//  upgrade: false
-//});
+// export const socket = io(URL, {
+//   transports: ['websocket'],
+//   upgrade: false
+// });
