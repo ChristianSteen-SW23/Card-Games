baa13f82f169eeb98618436f843debccf15c0fc9
Christian Steen Nielsen <cniel23@student.aau.dk>
2025-09-27 15:21:40 +0200

    Added the start for rust

---
+++ server-rust/src/main.rs
@@ -0,0 +1,23 @@
+use axum::routing::get;
+use socketioxide::{SocketIo, extract::SocketRef};
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (layer, io) = SocketIo::new_layer();
+
+    // Register a handler for the default namespace
+    io.ns("/", |s: SocketRef| {
+        // For each "message" event received, send a "message-back" event with the "Hello World!" event
+        s.on("message", |s: SocketRef| {
+            s.emit("message-back", "Hello World!").ok();
+        });
+    });
+
+    let app = axum::Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(layer);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
